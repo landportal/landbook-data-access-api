bc76e83faeb381f4f03f76ab7e37ed86114b833a
Herminio García <herminiogg@hotmail.com>
2014-03-11 17:55:47 +0100

    Closes #9

--- app/views.py
+++ app/views.py
@@ -9,14 +9,15 @@ from flask.helpers import url_for
 from flask import json
 from app import app
 from app.utils import JSONConverter, XMLConverter, DictionaryList2ObjectList
-from app.models import Country, Indicator, User
-from app.services import CountryService, IndicatorService, UserService
+from app.models import Country, Indicator, User, Organization
+from app.services import CountryService, IndicatorService, UserService, OrganizationService
 from flask import request
 
 api = Api(app)
 country_service = CountryService()
 indicator_service = IndicatorService()
 user_service = UserService()
+organization_service = OrganizationService()
 json_converter = JSONConverter()
 xml_converter = XMLConverter()
 list_converter = DictionaryList2ObjectList()
@@ -25,9 +26,9 @@ list_converter = DictionaryList2ObjectList()
 class CountryListAPI(Resource):
     '''
     Countries collection URI
-    Methods: GET, POST, PUT, DELETE  
+    Methods: GET, POST, PUT, DELETE
     '''
-    
+
     def get(self):
         '''
         List all countries
@@ -39,7 +40,7 @@ class CountryListAPI(Resource):
         else:
             return Response(json_converter.list_to_json(country_service.get_all()
                                                         ), mimetype='application/json')
-    
+
     def post(self):
         '''
         Create a new country
@@ -52,7 +53,7 @@ class CountryListAPI(Resource):
             country_service.insert(country)
             return {'URI': url_for('countries', code=country.iso3)}, 201 #returns the URI for the new country
         abort(400) # in case something is wrong
-    
+
     def put(self):
         '''
         Update all countries given
@@ -61,8 +62,8 @@ class CountryListAPI(Resource):
         country_list = json.loads(request.data)
         country_list = list_converter.convert(country_list)
         country_service.update_all(country_list)
-        return {}, 204 
-    
+        return {}, 204
+
     def delete(self):
         '''
         Delete all countries
@@ -71,13 +72,13 @@ class CountryListAPI(Resource):
         '''
         country_service.delete_all()
         return {}, 204
-        
+
 class CountryAPI(Resource):
     '''
     Countries element URI
     Methods: GET, PUT, DELETE
     '''
-    
+
     def get(self, code):
         '''
         Show country
@@ -92,7 +93,7 @@ class CountryAPI(Resource):
         else:
             return Response(json_converter.object_to_json(country
                                                           ), mimetype='application/json')
-    
+
     def put(self, code):
         '''
         If exists update country
@@ -109,7 +110,7 @@ class CountryAPI(Resource):
             return {}, 204
         else:
             abort(400)
-    
+
     def delete(self, code):
         '''
         Delete country
@@ -283,7 +284,7 @@ class UserAPI(Resource):
             abort(404)
         if is_xml_accepted(request):
             return Response(xml_converter.object_to_xml(user,
-                                                        'country'), mimetype='application/xml')
+                                                        'user'), mimetype='application/xml')
         else:
             return Response(json_converter.object_to_json(user
                                                           ), mimetype='application/json')
@@ -314,14 +315,158 @@ class UserAPI(Resource):
         user_service.delete(id)
         return {}, 204
 
-    
-api.add_resource(CountryListAPI, '/api/countries', endpoint = 'countries_list')
-api.add_resource(CountryAPI, '/api/countries/<code>', endpoint = 'countries')
-api.add_resource(IndicatorListAPI, '/api/indicators', endpoint = 'indicators_list')
-api.add_resource(IndicatorAPI, '/api/indicators/<id>', endpoint = 'indicators')
-api.add_resource(UserListAPI, '/api/users', endpoint = 'users_list')
-api.add_resource(UserAPI, '/api/users/<id>', endpoint = 'users')
-      
+class OrganizationListAPI(Resource):
+    '''
+    Organizations collection URI
+    Methods: GET, POST, PUT, DELETE
+    '''
+
+    def get(self):
+        '''
+        List all organizations
+        Response 200 OK
+        '''
+        if is_xml_accepted(request):
+            return Response(xml_converter.list_to_xml(organization_service.get_all(),
+                                                      'organizations', 'organization'), mimetype='application/xml')
+        else:
+            return Response(json_converter.list_to_json(organization_service.get_all()
+                                                        ), mimetype='application/json')
+
+    def post(self):
+        '''
+        Create a new organization
+        Response 201 CREATED
+        @return: URI
+        '''
+        organization = Organization(request.json.get("id"), request.json.get("name"),
+                                    request.json.get("url"), request.json.get("is_part_of_id"))
+        if organization.id is not None:
+            organization_service.insert(organization)
+            return {'URI': url_for('organizations', id=organization.id)}, 201  # returns the URI for the new user
+        abort(400)  # in case something is wrong
+
+    def put(self):
+        '''
+        Update all countries given
+        Response 204 NO CONTENT
+        '''
+        organization_list = json.loads(request.data)
+        organization_list = list_converter.convert(organization_list)
+        organization_service.update_all(organization_list)
+        return {}, 204
+
+    def delete(self):
+        '''
+        Delete all countries
+        Response 204 NO CONTENT
+        @attention: Take care of what you do, all countries will be destroyed
+        '''
+        organization_service.delete_all()
+        return {}, 204
+
+class OrganizationAPI(Resource):
+    '''
+    Countries element URI
+    Methods: GET, PUT, DELETE
+    '''
+
+    def get(self, id):
+        '''
+        Show country
+        Response 200 OK
+        '''
+        organization = organization_service.get_by_code(id)
+        if organization is None:
+            abort(404)
+        if is_xml_accepted(request):
+            return Response(xml_converter.object_to_xml(organization,
+                                                        'organization'), mimetype='application/xml')
+        else:
+            return Response(json_converter.object_to_json(organization
+                                                          ), mimetype='application/json')
+
+    def put(self, id):
+        '''
+        If exists update country
+        Response 204 NO CONTENT
+        If not
+        Response 400 BAD REQUEST
+        '''
+        organization = organization_service.get_by_code(id)
+        if organization is not None:
+            organization.id = request.json.get("id")
+            organization.name = request.json.get("name")
+            organization.url = request.json.get("url")
+            organization.is_part_of_id = request.json.get("is_part_of_id")
+            organization_service.update(organization)
+            return {}, 204
+        else:
+            abort(400)
+
+    def delete(self, id):
+        '''
+        Delete country
+        Response 204 NO CONTENT
+        '''
+        organization_service.delete(id)
+        return {}, 204
+
+class OrganizationUserListAPI(Resource):
+    '''
+    Organizations users collection URI
+    Methods: GET, POST, PUT, DELETE
+    '''
+
+    def get(self, organization_id):
+        '''
+        List all organizations
+        Response 200 OK
+        '''
+        if is_xml_accepted(request):
+            return Response(xml_converter.list_to_xml(organization_service.get_by_code(organization_id).users,
+                                                      'users', 'user'), mimetype='application/xml')
+        else:
+            return Response(json_converter.list_to_json(organization_service.get_by_code(organization_id).users
+                                                        ), mimetype='application/json')
+
+
+class OrganizationUserAPI(Resource):
+    '''
+    Countries element URI
+    Methods: GET, PUT, DELETE
+    '''
+
+    def get(self, organization_id, user_id):
+        '''
+        Show country
+        Response 200 OK
+        '''
+        selected_user = None
+        for user in organization_service.get_by_code(organization_id).users:
+            if user.id == int(user_id):
+                selected_user = user
+        if selected_user is None:
+            abort(404)
+        if is_xml_accepted(request):
+            return Response(xml_converter.object_to_xml(selected_user,
+                                                        'user'), mimetype='application/xml')
+        else:
+            return Response(json_converter.object_to_json(selected_user
+                                                          ), mimetype='application/json')
+
+
+api.add_resource(CountryListAPI, '/api/countries', endpoint='countries_list')
+api.add_resource(CountryAPI, '/api/countries/<code>', endpoint='countries')
+api.add_resource(IndicatorListAPI, '/api/indicators', endpoint='indicators_list')
+api.add_resource(IndicatorAPI, '/api/indicators/<id>', endpoint='indicators')
+api.add_resource(UserListAPI, '/api/users', endpoint='users_list')
+api.add_resource(UserAPI, '/api/users/<id>', endpoint='users')
+api.add_resource(OrganizationListAPI, '/api/organizations', endpoint='organizations_list')
+api.add_resource(OrganizationAPI, '/api/organizations/<id>', endpoint='organizations')
+api.add_resource(OrganizationUserListAPI, '/api/organizations/<organization_id>/users', endpoint='organizations_users_list')
+api.add_resource(OrganizationUserAPI, '/api/organizations/<organization_id>/users/<user_id>', endpoint='organizations_users')
+
 def is_xml_accepted(request):
     '''
     Returns if json is accepted or not, returns json as default
