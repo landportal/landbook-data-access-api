cc0c1f3c2af31f200cad80255263cd7eb2f03020
Herminio García <herminiogg@hotmail.com>
2014-05-28 12:16:24 +0200

    Fix CI

--- app/__init__.py
+++ app/__init__.py
@@ -11,15 +11,15 @@ from flask.ext.track_usage.storage.sql import SQLStorage
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/landportal'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foo.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/landportal'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foo.db'
 app.config['TRACK_USAGE_USE_FREEGEOIP'] = False
 app.config['TRACK_USAGE_INCLUDE_OR_EXCLUDE_VIEWS'] = 'exclude'
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
-sql_database_storage = SQLStorage('mysql+mysqlconnector://root:root@localhost:3306/landportal', table_name='api_usage')
-#sql_database_storage = SQLStorage('sqlite:///analytics.db', table_name='api_usage')
+#sql_database_storage = SQLStorage('mysql+mysqlconnector://root:root@localhost:3306/landportal', table_name='api_usage')
+sql_database_storage = SQLStorage('sqlite:///analytics.db', table_name='api_usage')
 t = TrackUsage(app, sql_database_storage)
 
 from app import views
