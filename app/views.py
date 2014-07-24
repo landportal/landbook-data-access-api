c451a3c9568f48e88cea87b571be69a967a4d87b
Herminio García <herminiogg@hotmail.com>
2014-07-24 12:51:16 +0200

    Added GPL license

--- app/views.py
+++ app/views.py
@@ -1,8 +1,29 @@
+# landportal-data-access-api
+# Copyright (c)2014, WESO, Web Semantics Oviedo.
+# Written by Herminio García.
+
+# This file is part of landportal-data-access-api.
+#
+# landportal-data-access-api is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License.
+#
+# landportal-data-access-api is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with landportal-data-access-api.  If not, see <http://www.gnu.org/licenses/>.
+
+# landportal-data-access-api is licensed under the terms of the GPLv2
+# <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+
 """
 Created on 03/02/2014
 This file includes all the URI implementation and calls
 
-:author: Weso
+:author: Herminio García
 """
 from itertools import groupby
 import urllib2
